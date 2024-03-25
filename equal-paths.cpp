ac1f4ee6218e7b56ad6aad4df4857d3a8e4fa6ad
Brandon Wong <bwong109@usc.edu>
2024-03-25 20:46:45 +0000

    woo

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,49 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int height (Node *root){
+    // Check if null
+    if (root == nullptr){
+        return 0;
+    }
+    else{
+        // Get height of left and right
+        int leftHeight = height(root -> left);
+        int rightHeight = height(root -> right);
+
+        // Return the longer height 
+        if (leftHeight > rightHeight) {
+            return leftHeight + 1;
+        }
+        else {
+            return rightHeight + 1;
+        }
+    }
+
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    // Check if root is null or leaf node
+    if (root == nullptr || (root->left == nullptr && root->right == nullptr)) {
+        return true;
+    }
 
+    // Check if only one subtree is present
+    if (root->left == nullptr) {
+        return height(root->right) == 1;
+    }
+    if (root->right == nullptr) {
+        return height(root->left) == 1;
+    }
+
+    // Check if paths are of equal length
+    if (height(root->left) == height(root->right)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
 
